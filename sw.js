dd6b40087647413845172e6c5a66a21113197dc5
thomaz310313 <93798663+thomaz310313@users.noreply.github.com>
2026-06-16 10:24:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('zara-fm-store').then((cache) => cache.addAll(['/']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
