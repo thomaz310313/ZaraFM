327864414dd23997a07257ef6b49cb4c18448c9d
thomaz310313 <93798663+thomaz310313@users.noreply.github.com>
2026-08-31 10:01:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,13 @@
-var CACHE_NAME = 'zarafm-v0.27';
-
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function(e) {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
                 cacheNames.map(function(cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
+                    return caches.delete(cacheName);
                 })
             );
         }).then(function() {
@@ -20,12 +16,6 @@ self.addEventListener('activate', function(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    if (event.request.mode === 'navigate' || event.request.destination === 'document') {
-        event.respondWith(
-            fetch(event.request).catch(function() {
-                return caches.match(event.request);
-            })
-        );
-    }
+self.addEventListener('fetch', function(e) {
+    e.respondWith(fetch(e.request));
 });
