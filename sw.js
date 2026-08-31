398eb160cd1319ecf42537d72e632cddd329d76a
thomaz310313 <93798663+thomaz310313@users.noreply.github.com>
2026-08-31 09:59:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('zara-fm-store').then((cache) => cache.addAll(['/']))
-  );
+var CACHE_NAME = 'zarafm-v0.27';
+
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(function() {
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    if (event.request.mode === 'navigate' || event.request.destination === 'document') {
+        event.respondWith(
+            fetch(event.request).catch(function() {
+                return caches.match(event.request);
+            })
+        );
+    }
 });
